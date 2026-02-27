418cfb6b906b50de9d95a9daf1adfbc158625a09
Catslife66 <josie58@live.cn>
2026-02-27 13:28:37 +0000

    Added suspense to task

--- frontend/app/tasks/page.tsx
+++ frontend/app/tasks/page.tsx
@@ -8,7 +8,7 @@ import TaskCard from "@/components/TaskCard";
 import TaskForm from "@/components/TaskForm";
 import { ApiError, apiFetch } from "@/lib/api";
 import { PaginatedTask, Task } from "@/lib/types";
-import { useEffect, useState, Fragment } from "react";
+import { useEffect, useState, Fragment, Suspense } from "react";
 
 const FETCH_TASKS_API_PATH = "/api/tasks";
 
@@ -76,94 +76,96 @@ export default function page() {
   }, [page]);
 
   return (
-    <RequireAuth>
-      <div className="w-xl flex flex-col items-center mx-auto py-8 px-4 pt-16">
-        <h1 className="text-xl font-bold my-8">My Tasks</h1>
-        <div className="w-full px-4">
-          <TaskForm
-            mode="create"
-            onCreated={() => {
-              showSuccessMsg("Task is created.");
-              fetchTasks();
-            }}
-          />
-          {error && <ErrorAlert errMsg={error} />}
+    <Suspense fallback={null}>
+      <RequireAuth>
+        <div className="w-xl flex flex-col items-center mx-auto py-8 px-4 pt-16">
+          <h1 className="text-xl font-bold my-8">My Tasks</h1>
+          <div className="w-full px-4">
+            <TaskForm
+              mode="create"
+              onCreated={() => {
+                showSuccessMsg("Task is created.");
+                fetchTasks();
+              }}
+            />
+            {error && <ErrorAlert errMsg={error} />}
 
-          {successMsg && <SuccessToast msg={successMsg} />}
+            {successMsg && <SuccessToast msg={successMsg} />}
 
-          <div className="flex flex-col space-y-4">
-            {loading && <div>Loading tasks...</div>}
-            {tasks.length > 0 ? (
-              tasks.map((task, idx) => (
-                <Fragment key={task.id}>
-                  {editingId === task.id ? (
-                    <TaskForm
-                      mode="update"
-                      taskId={task.id}
-                      initValues={{
-                        title: task.title,
-                        status: task.status,
-                        priority: task.priority,
-                        due_datetime: task.due_datetime,
-                      }}
-                      onSave={() => {
-                        setEditingId(null);
-                        fetchTasks();
-                        showSuccessMsg("Task is updated.");
-                      }}
-                      onCancel={() => setEditingId(null)}
-                    />
-                  ) : (
-                    <div className="flex flex-row items-center justify-center">
-                      <div
-                        onClick={() => setEditingId(task.id)}
-                        className="cursor-pointer w-lg flex items-center px-4 py-2 border border-gray-200 rounded-lg dark:border-gray-700"
-                        tabIndex={idx}
-                      >
-                        <TaskCard task={task} />
-                      </div>
-
-                      <button
-                        onClick={async () => {
-                          await deleteTask(task.id);
+            <div className="flex flex-col space-y-4">
+              {loading && <div>Loading tasks...</div>}
+              {tasks.length > 0 ? (
+                tasks.map((task, idx) => (
+                  <Fragment key={task.id}>
+                    {editingId === task.id ? (
+                      <TaskForm
+                        mode="update"
+                        taskId={task.id}
+                        initValues={{
+                          title: task.title,
+                          status: task.status,
+                          priority: task.priority,
+                          due_datetime: task.due_datetime,
                         }}
-                        className="ms-4 cursor-pointer border border-gray-200 rounded-full p-2 hover:bg-gray-200"
-                      >
-                        <svg
-                          className="w-4 h-4 text-gray-800 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          fill="none"
-                          viewBox="0 0 24 24"
+                        onSave={() => {
+                          setEditingId(null);
+                          fetchTasks();
+                          showSuccessMsg("Task is updated.");
+                        }}
+                        onCancel={() => setEditingId(null)}
+                      />
+                    ) : (
+                      <div className="flex flex-row items-center justify-center">
+                        <div
+                          onClick={() => setEditingId(task.id)}
+                          className="cursor-pointer w-lg flex items-center px-4 py-2 border border-gray-200 rounded-lg dark:border-gray-700"
+                          tabIndex={idx}
                         >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18 17.94 6M18 18 6.06 6"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  )}
-                </Fragment>
-              ))
-            ) : (
-              <div className="text-center text-xl">
-                You currently don't have any tasks.
-              </div>
-            )}
+                          <TaskCard task={task} />
+                        </div>
+
+                        <button
+                          onClick={async () => {
+                            await deleteTask(task.id);
+                          }}
+                          className="ms-4 cursor-pointer border border-gray-200 rounded-full p-2 hover:bg-gray-200"
+                        >
+                          <svg
+                            className="w-4 h-4 text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18 17.94 6M18 18 6.06 6"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    )}
+                  </Fragment>
+                ))
+              ) : (
+                <div className="text-center text-xl">
+                  You currently don't have any tasks.
+                </div>
+              )}
+            </div>
           </div>
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            onPageChange={changePage}
+          />
         </div>
-        <Pagination
-          page={page}
-          totalPages={totalPages}
-          onPageChange={changePage}
-        />
-      </div>
-    </RequireAuth>
+      </RequireAuth>
+    </Suspense>
   );
 }
