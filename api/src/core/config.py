59e816381474a992344aec6b36657f2d5bc66844
Catslife66 <josie58@live.cn>
2026-03-02 13:44:10 +0000

    Updated cookie config

--- api/src/core/config.py
+++ api/src/core/config.py
@@ -2,7 +2,7 @@ from decouple import config, Csv
 
 ENV = config("ENV", default='dev')
 COOKIE_SECURE = ENV == "prod"
-COOKIE_SAMESITE = config("COOKIE_SAMESITE", cast=lambda v: None if v.lower() == 'none' else v, default="lax")
+COOKIE_SAMESITE = config("COOKIE_SAMESITE", default="lax")
 SESSION_COOKIE_NAME = config("SESSION_COOKIE_NAME")
 SESSION_TTL_SECONDS = config("SESSION_TTL_SECONDS", cast=int, default=259200)
 DOMAIN_NAME = config("DOMAIN_NAME")
